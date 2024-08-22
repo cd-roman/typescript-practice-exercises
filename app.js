1f455e50695e6747e4b0266b3117ba90914a7225
Roman <60399380+cd-roman@users.noreply.github.com>
2024-08-22 22:04:34 +0200

    Add Intersection types examples

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("app.ts successfully compiled");
+const e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+console.log(e1);
--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
 console.log("app.ts successfully compiled");
+
+////////////////////////// Intersection Types //////////////////////////
+
+// Intersection types allow us to combine multiple types into one.
+
+// Example 1
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// Intersection type
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+console.log(e1);
+
+// Example 2
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+// Intersection type
+type Universal = Combinable & Numeric;
