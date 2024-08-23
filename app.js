a5f9f205cec6cf870b0a36f90107cc302131ff58
Roman <60399380+cd-roman@users.noreply.github.com>
2024-08-23 02:44:23 +0200

    Add generic basics and generic function examples

--- app.js
+++ app.js
@@ -1,110 +1,27 @@
 "use strict";
-var _a;
 console.log("app.ts successfully compiled");
-const e1 = {
-    name: "Max",
-    privileges: ["create-server"],
-    startDate: new Date(),
-};
-console.log(e1);
-////////////////////////// Type Guards //////////////////////////
-// Type guards allow us to check the type of a variable at runtime.
-function add(a, b) {
-    // Type guard
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+//////////////////////// GENERICS ////////////////////////
+// Generics are a way to make components reusable, by allowing them to work with any data type.
+// Example 1: Built-in Generics
+// Array is a built-in generic type in TypeScript
+const names = []; // same as const names: string[] = [];
+// Promise is also a built-in generic type in TypeScript
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("This is done!");
+    }, 2000);
+});
+promise.then((data) => {
+    console.log(data.split(" "));
+});
+// Example 2: Creating a Generic Function
+// The T and U are placeholders for the types that will be passed to the function when it is called
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-// We can use a "in" keyword to check if a property exists in an object.
-// The "in" keyword is accessible in the JavaScript runtime.
-function printEmployeeInformation(emp) {
-    console.log("Name: " + emp.name);
-    // Type guard
-    if ("privileges" in emp) {
-        console.log("Privileges: " + emp.privileges);
-    }
-    // Type guard
-    if ("startDate" in emp) {
-        console.log("Start Date: " + emp.startDate);
-    }
-}
-printEmployeeInformation(e1);
-// Type guard using instanceof for classes
-// instanceof is a JavaScript feature that allows us to check if an object is an instance of a class.
-class Car {
-    drive() {
-        console.log("Driving...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving a truck...");
-    }
-    loadCargo(amount) {
-        console.log("Loading cargo..." + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    // Type guard
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log("Moving with speed: " + speed);
-}
-const horse = { type: "horse", runningSpeed: 15 };
-moveAnimal(horse);
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-////////////////////////// Type Casting //////////////////////////
-// Type casting allows us to tell TypeScript that a certain value is of a certain type.
-// Example 1
-const paragraph = document.querySelector("p");
-const paragraphId = document.getElementById("message-output");
-// Type casting
-const userInputElement = (document.getElementById("user-input"));
-userInputElement.value = "Hi there!";
-// Alternative syntax
-const userInputElement2 = document.getElementById("user-input");
-userInputElement2.value = "Hi there!";
-const errorBag = {
-    email: "Not a valid email!",
-    username: "Must start with a capital character!",
-};
-function addFn(a, b) {
-    // Type guard
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const result = addFn(1, 5);
-const result2 = addFn("Max", " Schwarz");
-////////////////////////// Optional Chaining //////////////////////////
-// Optional chaining allows us to access properties of an object that might be undefined.
-const fetchedUserData = {
-    id: "u1",
-    name: "Max",
-    job: { title: "CEO", description: "My own company" },
-};
-// Optional chaining
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-////////////////////////// Nullish Coalescing //////////////////////////
-// Nullish coalescing allows us to set a default value if a variable is null or undefined.
-const userInput = null;
-const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
+// Now we can call the function with any type of object
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+const mergedObj2 = merge({ name: "Joey", hobbies: ["Sports"] }, { age: 32 });
+console.log(mergedObj.name); // Output: Max
+console.log(mergedObj.age); // Output: 30
+console.log(mergedObj2.name); // Output: Joey
--- app.ts
+++ app.ts
@@ -1,1 +1,37 @@
 console.log("app.ts successfully compiled");
+
+//////////////////////// GENERICS ////////////////////////
+
+// Generics are a way to make components reusable, by allowing them to work with any data type.
+
+// Example 1: Built-in Generics
+
+// Array is a built-in generic type in TypeScript
+const names: Array<string> = []; // same as const names: string[] = [];
+
+// Promise is also a built-in generic type in TypeScript
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  console.log(data.split(" "));
+});
+
+// Example 2: Creating a Generic Function
+
+// The T and U are placeholders for the types that will be passed to the function when it is called
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// Now we can call the function with any type of object
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+const mergedObj2 = merge({ name: "Joey", hobbies: ["Sports"] }, { age: 32 });
+
+console.log(mergedObj.name); // Output: Max
+console.log(mergedObj.age); // Output: 30
+console.log(mergedObj2.name); // Output: Joey
